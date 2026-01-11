3ee92894dea02ad8cd9294711aad61ab86919b4b
Faith Murrell <fnmurrell@gmail.com>
2026-01-11 15:35:45 -0500

    final web scrapper code

--- Web_Scrapper/main.py
+++ Web_Scrapper/main.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import itertools
+import json
 import time
 
 def scrape_page(driver):
@@ -16,14 +15,20 @@ def scrape_page(driver):
     for entry in review_entries:
         user = entry.find_element(By.CLASS_NAME,"ReviewerProfile__name").text
 
-        rating_element = entry.find_element(By.CSS_SELECTOR, "span.RatingStars")
-        rating = int(rating_element.get_attribute("aria-label").split()[1])
+        try:
+            rating_element = entry.find_element(By.CSS_SELECTOR, "span.RatingStars")
+            rating = int(rating_element.get_attribute("aria-label").split()[1])
+        except:
+            rating = None
 
         comment = entry.find_element(By.CLASS_NAME,"ReviewText").text
         
         date = entry.find_element(By.XPATH,'.//span[contains(@class,"Text__body3")]/a').text
      
-        likes = entry.find_element(By.XPATH,'.//span[contains(@class, "Button__labelItem") and contains(normalize-space(.), "like")]').text
+        try:
+            likes = entry.find_element(By.XPATH,'.//span[contains(@class, "Button__labelItem") and contains(normalize-space(.), "like")]').text
+        except:
+            likes = 0
         
         reviews.append({
             "user": user,
@@ -40,12 +45,14 @@ def load_next_page(driver):
     button = driver.find_element(By.CSS_SELECTOR,"[data-testid='loadMore']")
     button.click()
 
-def write_to_file(reviews,filename):
-    pass
+def write_to_file(reviews, filename):
+    with open(filename, "w") as final:
+        json.dump(reviews, final, indent=2, default=lambda x: list(x) if isinstance(x, tuple) else str(x))
+    return print("Data written to JSON.")
 
-NUM_PAGES = 5 #10850 / 30
+NUM_PAGES = 10856 // 30
 OUTPUT_FILE = "goodreads_reviews.json"
-TIME_SLEEP = 3
+TIME_SLEEP = 5
 URL = "https://www.goodreads.com/book/show/46787/reviews?reviewFilters=eyJhZnRlciI6Ik9UTXdOeXd4TlRRME16RTNPVFEyTWpReiJ9"
 
 def main():
@@ -61,7 +68,7 @@ def main():
     
     # loop through review pages, scrape data, append, and click button for more reviews
     for page in range(0,NUM_PAGES):
-        all_reviews.extend(scrape_page(driver))
+        all_reviews.append(scrape_page(driver))
         load_next_page(driver)
         time.sleep(TIME_SLEEP)
 
@@ -70,7 +77,6 @@ def main():
 
     # Flatten the reviews list
     final_reviews = list(itertools.chain.from_iterable(all_reviews))
-    print(final_reviews)
 
     # Generate final dataset
     write_to_file(final_reviews,OUTPUT_FILE)
